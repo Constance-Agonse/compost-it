9e19155c694d210f951f9de75939b56dd4f6a371
Constance <constance@MacBook-Pro-de-Constance.local>
2021-10-08 07:51:32 +0200

    fixing sounds

--- index.js
+++ index.js
@@ -28,8 +28,15 @@ const playAgain = document.getElementById('play-again');
 let rewardSentence = document.getElementById('reward-sentence');
 let congratsWord = document.getElementById('congrats-word');
 const startButton = document.getElementById('start-btn');
-let collideMusic = document.getElementById('collide-music');
 let scoreGameContainer = document.getElementById('score-game-container');
+// Sounds
+const miamSound = document.getElementById('miam-sound');
+const winGameSound = document.getElementById('win-game-sound');
+const looseGameSound = document.getElementById('loose-game-sound');
+const goodItem = document.getElementById('good-item');
+const badItem = document.getElementById('bad-item');
+const nautralItem = document.getElementById('neutral-item');
+const buttonSond = document.getElementById('button-sound');
 
 const state = {
   basketXPosition: screenLimitWidth / 2 - BASKET_WIDTH / 2,
@@ -45,25 +52,22 @@ const endGame = () => {
   gameContainer.style.display = 'none';
   clearInterval(countdownInterval);
   chronometer.stop();
-  collideMusic.innerHTML = '';
 
   //instead of removing, move to style.display='none'
   // basket.remove();
   basket.style.display = 'none';
 
-  const miamSound = document.getElementById('miam-sound');
   miamSound.play();
 
   //au bout de 5 secondes, le maturation stage disparait, si on a gagné la winning page s'affiche, 
   //si on a perdu, la losing page s'affiche
-  console.log(state.totalScore);
   setTimeout(() => {
     // la page maturation stage est visible pendant 5 secondes
     maturationStage.style.display = 'none';
     if (state.totalScore > 20) {
       endPage.style.backgroundImage = `url('./images/YOUWON.png')`;
-
-      rewardSentence.innerHTML = 'You are a compost master';
+      winGameSound.play();
+      rewardSentence.innerHTML = 'You are a compost master!';
       congratsWord.innerHTML = 'Yeah! ';
       endPage.style.display = 'block';
       scorewHTML.innerHTML = state.totalScore;
@@ -73,7 +77,8 @@ const endGame = () => {
       endPage.style.display = 'block';
       congratsWord.innerHTML = 'Oups! '
       scorewHTML.innerHTML = state.totalScore;
-      endPage.style.backgroundImage = `url('./images/YOULOSE.png')`
+      endPage.style.backgroundImage = `url('./images/YOULOSE.png')`;
+      looseGameSound.play();
       rewardSentence.innerHTML = 'Try again, you are not gonna save the planet at this pace!';
     };
 
@@ -94,6 +99,7 @@ const updateCountDown = () => {
 
 //QUAND ON PRESSE LE START BTN
 startButton.addEventListener('click', () => {
+  buttonSond.play();
   startScreenContainer.style.display = 'none';
   startGame();
   setTimeout(() => {
@@ -121,17 +127,14 @@ const startGame = () => {
     wasteItem.makeItRain((wasteItemYPosition, wasteItemXPosition) => {
       if (isCollided(wasteItemYPosition, wasteItemXPosition)) {
         if (wasteItem.category === 'good') {
-          collideMusic.innerHTML = `<audio controls autoplay style="display: none">
-        <source src="./sounds/242501__gabrielaraujo__powerup-success.wav" type="audio/mpeg" >
-        </audio>`};
+          goodItem.play()
+        };
         if (wasteItem.category === 'bad') {
-          collideMusic.innerHTML = `<audio controls autoplay style="display: none">
-        <source src="sounds/457752__tissman__fail4.wav" type="audio/mpeg" >
-        </audio>`};
+          badItem.play();
+        };
         if (wasteItem.category === 'dunno') {
-          collideMusic.innerHTML = `<audio controls autoplay style="display: none">
-        <source src="sounds/neutralsong.wav" >
-        </audio>`};
+          nautralItem.play();
+        };
         state.totalScore += wasteItem.score;
         wasteItem.removeWaste();
       }
@@ -155,34 +158,13 @@ const startGame = () => {
   });
 }
 
-// //VERIFIER LA COLLISION
-// const isCollided = (wasteItemYPosition, wasteItemXPosition) => {
-//   const collisionY = window.innerHeight - BASKET_HEIGHT + 70;
-//   const wasteItemWidth = 80;
-//   const wasteItemHeight = 80;
-//   if (state.basketXPosition < wasteItemXPosition + wasteItemWidth &&
-//     state.basketXPosition + BASKET_WIDTH > wasteItemXPosition &&
-//     collisionY < wasteItemYPosition + wasteItemHeight &&
-//     collisionY + collisionY > wasteItemYPosition) {
-//     return true
-//   } else {
-//     return false;
-//   }
-// };
+
 
 //VERIFIER LA COLLISION
 const isCollided = (wasteItemYPosition, wasteItemXPosition) => {
   const collisionY = window.innerHeight - BASKET_HEIGHT + 70;
   const wasteItemWidth = 70;
   const wasteItemHeight = 70;
-  // if (state.basketXPosition < wasteItemXPosition + wasteItemWidth &&
-  //   state.basketXPosition + BASKET_WIDTH > wasteItemXPosition &&
-  //   collisionY < wasteItemYPosition + wasteItemHeight &&
-  //   collisionY + collisionY > wasteItemYPosition) {
-  //   return true
-  // } else {
-  //   return false;
-  // }
   const collisionLineA = window.innerHeight - BASKET_HEIGHT;
   const collisionLineB = window.innerHeight - 120;
 
@@ -199,7 +181,7 @@ const isCollided = (wasteItemYPosition, wasteItemXPosition) => {
 ///SET LE BOUTON DE PLAY AGAIN
 playAgain.addEventListener('click', () => {
   endPage.style.display = 'none';
-
+  buttonSond.play();
   startGame();
   setTimeout(() => {
     endGame();
